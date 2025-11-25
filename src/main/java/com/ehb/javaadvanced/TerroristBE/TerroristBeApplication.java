6616ced27ccb634b73a8c04d8417f43c9aa3ad21
Dominique Callaert <dominique.callaert@arag.be>
2025-11-25 18:17:40 +0100

    Initial repository with original generated start.spring.io project

---
+++ src/main/java/com/ehb/javaadvanced/TerroristBE/TerroristBeApplication.java
@@ -0,0 +1,13 @@
+package com.ehb.javaadvanced.TerroristBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerroristBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerroristBeApplication.class, args);
+	}
+
+}
